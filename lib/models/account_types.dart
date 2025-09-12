c06787e1eba5e716426f18a5c3a36fe5081268ba
Alessio Caputo <contact@alessioc42.dev>
2025-09-12 17:50:35 +0200

    feat: add account type label on drawer

--- lib/models/account_types.dart
+++ lib/models/account_types.dart
@@ -1,5 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:lanis/generated/l10n.dart';
+
 enum AccountType {
   student,
   teacher,
   parent,
+}
+
+extension AccountTypeExtension on AccountType {
+  String readableName(BuildContext context) {
+    switch (this) {
+      case AccountType.student:
+        return AppLocalizations.of(context).student;
+      case AccountType.teacher:
+        return AppLocalizations.of(context).teacher;
+      case AccountType.parent:
+        return AppLocalizations.of(context).parent;
+    }
+  }
 }
