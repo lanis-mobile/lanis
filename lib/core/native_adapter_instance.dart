bcca24e23bd17b55b8598a5ba0c1670f7e2f39f5
Simon <75274054+Yoshlix@users.noreply.github.com>
2025-12-14 16:35:08 +0100

    revert fallback

--- lib/core/native_adapter_instance.dart
+++ lib/core/native_adapter_instance.dart
@@ -1,27 +1,19 @@
-import 'package:dio/dio.dart';
-import 'package:dio/io.dart';
 import 'package:native_dio_adapter/native_dio_adapter.dart';
 
-HttpClientAdapter? _cachedAdapter;
+NativeAdapter? _cachedNativeAdapter;
 CronetEngine? _cachedCronetEngine;
 
-HttpClientAdapter getNativeAdapterInstance() {
-  if (_cachedAdapter != null) return _cachedAdapter!;
-
-  try {
-    _cachedAdapter = NativeAdapter(
-      createCronetEngine: () {
-        _cachedCronetEngine ??= CronetEngine.build(
-          enableHttp2: true,
-          enableBrotli: true,
-          enableQuic: true,
-        );
-        return _cachedCronetEngine!;
-      },
-    );
-  } catch (e) {
-    _cachedAdapter = IOHttpClientAdapter();
-  }
-
-  return _cachedAdapter!;
+NativeAdapter getNativeAdapterInstance() {
+  if (_cachedNativeAdapter != null) return _cachedNativeAdapter!;
+  _cachedNativeAdapter = NativeAdapter(
+    createCronetEngine: () {
+      _cachedCronetEngine ??= CronetEngine.build(
+        enableHttp2: true,
+        enableBrotli: true,
+        enableQuic: true,
+      );
+      return _cachedCronetEngine!;
+    },
+  );
+  return _cachedNativeAdapter!;
 }
