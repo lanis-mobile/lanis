94a1f9325d300d0f0003315c0b0c66e2c55148a6
Stefan Friedrich <friedrich.stef@icloud.com>
2026-06-03 19:30:27 +0200

    fix(widgets): lower LanisWidgetsBundle availability to iOS 16.0

--- ios/LanisWidgets/LanisWidgetsBundle.swift
+++ ios/LanisWidgets/LanisWidgetsBundle.swift
@@ -2,7 +2,7 @@ import WidgetKit
 import SwiftUI
 
 @main
-@available(iOSApplicationExtension 17.0, *)
+@available(iOSApplicationExtension 16.0, *)
 struct LanisWidgetsBundle: WidgetBundle {
     var body: some Widget {
         StundenplanWidget()
