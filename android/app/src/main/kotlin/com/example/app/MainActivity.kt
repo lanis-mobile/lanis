54561064a423f9a8864d066f157507622c8ec1cd
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2026-07-30 00:42:48 +0200

    doc scanner cleanup

--- android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.app
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
-import com.zynksoftware.documentscanner.ui.DocumentScanner
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -15,9 +12,7 @@ import java.io.IOException
 
 class MainActivity: FlutterActivity() {
     private val createFileCode = 1404
-    private val scanDocumentCode = 4200
     private var filePath = ""
-    private var scanDocumentCallback: ((Uri?) -> Unit)? = null
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -27,15 +22,6 @@ class MainActivity: FlutterActivity() {
                 "saveFile" -> {
                     filePath = call.argument<String>("filePath").toString()
                     createFile(call.argument<String>("fileName").toString(), call.argument<String>("mimeType").toString())
-                }
-                "scanDocument" -> {
-                    scanDocument { uri ->
-                        if (uri != null) {
-                            result.success(uri.path)
-                        } else {
-                            result.success(null)
-                        }
-                    }
                 }
                 else -> {
                     result.notImplemented()
@@ -64,15 +50,6 @@ class MainActivity: FlutterActivity() {
                     }
                 }
             }
-
-            scanDocumentCode -> {
-                data?.data?.let { uri ->
-                    scanDocumentCallback?.let { callback ->
-                        callback(uri)
-                        scanDocumentCallback = null
-                    }
-                }
-            }
         }
     }
 
@@ -83,22 +60,6 @@ class MainActivity: FlutterActivity() {
             putExtra(Intent.EXTRA_TITLE, fileName)
         }
         startActivityForResult(intent, createFileCode)
-    }
-
-    /**
-     * Returns the file path
-     */
-    private fun scanDocument(callback: (Uri?) -> Unit) {
-        val configuration = DocumentScanner.Configuration()
-        configuration.imageQuality = 100
-        configuration.imageType = Bitmap.CompressFormat.PNG
-        configuration.galleryButtonEnabled = false // Is buggy with permissions
-        DocumentScanner.init(this, configuration)
-
-        val intent = Intent(this, AppScanActivity::class.java)
-        startActivityForResult(intent, scanDocumentCode)
-
-        scanDocumentCallback = callback
     }
 
     companion object {
